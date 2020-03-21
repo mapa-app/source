7bac9fcd564db2a11d7e34d3d0b6e2b49a97d92c
David Enke <david@enke.dev>
2020-03-21 23:24:02 +0100

    fixed netlify lambda babel stack

--- packages/backend/src/functions/apollo-graphql.ts
+++ packages/backend/src/functions/apollo-graphql.ts
@@ -1,23 +1,23 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
-import mongoose from 'mongoose';
+import { ApolloServer } from 'apollo-server-lambda';
+import { connect } from 'mongoose';
 import { linkSchema } from '../schemas/';
 import { schema } from '../schemas/schema';
 import { userResolver } from '../resolvers/user.resolver';
 
 const server = new ApolloServer({
- typeDefs: [
+  typeDefs: [
     linkSchema,
     schema
-    ],
-    resolvers: [
-      userResolver
-    ],
-})
+  ],
+  resolvers: [
+    userResolver
+  ]
+});
 
-mongoose.connect('mongodb://ds233596.mlab.com:33596/mapa', {
+connect('mongodb://ds233596.mlab.com:33596/mapa', {
   // TODO: use credentials from env vars!
   user: 'mapa',
   pass: 'i9-Af4xeq57C#34'
 });
 
-exports.handler = server.createHandler()
+export const handler = server.createHandler();
