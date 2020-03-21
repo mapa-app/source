0bc782a738371e3443fdc9056635fa76470cae65
Christian Klauss <mail@christianklauss.name>
2020-03-22 00:47:21 +0100

    Update apollo-graphql.ts
    See if removing mongoose changes anything

--- packages/backend/src/functions/apollo-graphql.ts
+++ packages/backend/src/functions/apollo-graphql.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from 'apollo-server-lambda';
-import { connect } from 'mongoose';
+// import { connect } from 'mongoose';
 import { linkSchema } from '../schemas/';
 import { schema } from '../schemas/schema';
 import { userResolver } from '../resolvers/user.resolver';
@@ -18,18 +18,18 @@ export const handler = server.createHandler();
 
 console.log('created handler');
 
-connect('mongodb://ds233596.mlab.com:33596/mapa', {
-  // TODO: use credentials from env vars!
-  user: 'mapa',
-  pass: 'i9-Af4xeq57C#34',
-  bufferCommands: false,
-  bufferMaxEntries: 0,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-
-}).then(mongoose => {
-  console.log('connected to mongodb');
-}).catch(e => {
-  console.error('error connecting to mongodb', e)
-  process.exit()
-})
+// connect('mongodb://ds233596.mlab.com:33596/mapa', {
+//   // TODO: use credentials from env vars!
+//   user: 'mapa',
+//   pass: 'i9-Af4xeq57C#34',
+//   bufferCommands: false,
+//   bufferMaxEntries: 0,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+//
+// }).then(mongoose => {
+//   console.log('connected to mongodb');
+// }).catch(e => {
+//   console.error('error connecting to mongodb', e)
+//   process.exit()
+// })
