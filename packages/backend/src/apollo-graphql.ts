7d80788515a9fef4ae08387ffaee0d42fcbf35ae
Christian Klauss <mail@christianklauss.name>
2020-03-22 13:30:57 +0100

    Add docker-compose file that includes mongoDB

--- packages/backend/src/apollo-graphql.ts
+++ packages/backend/src/apollo-graphql.ts
@@ -7,28 +7,32 @@ import { linkSchema } from './schemas/';
 import { schema } from './schemas/schema';
 import { userResolver } from './resolvers/user.resolver';
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
+//
+// const server = new ApolloServer({
+//   playground: true,
+//   typeDefs: [
+//     linkSchema,
+//     schema
+//   ],
+//   resolvers: [
+//     userResolver
+//   ]
+// });
+//
+// server.applyMiddleware({ app, path: '/graphql' });
 
-const server = new ApolloServer({
-  playground: true,
-  typeDefs: [
-    linkSchema,
-    schema
-  ],
-  resolvers: [
-    userResolver
-  ]
-});
-
-server.applyMiddleware({ app, path: '/graphql' });
-
-app.listen(4000, async () => {
-  await connect('mongodb://ds233596.mlab.com:33596/mapa', {
+// app.listen(4000, async () => {
+  connect('mongodb://localhost:27017', {
     // TODO: use credentials from env vars!
     user: 'mapa',
     pass: 'i9-Af4xeq57C#34',
     useNewUrlParser: true,
     useUnifiedTopology: true
+  }).then(c => {
+    console.log('success')
+  }).catch(e => {
+    console.log(e);
   });
-});
+// });
