5a0dc5b4361b2ecd7b0399e90026cb1f59eb6b4a
David Enke <david@enke.dev>
2020-03-22 15:28:08 +0100

    positioned entries

--- packages/frontend/src/components/features/diary/diary.tsx
+++ packages/frontend/src/components/features/diary/diary.tsx
@@ -1,4 +1,4 @@
-import { Component, ComponentInterface, h } from '@stencil/core';
+import { Component, ComponentInterface, Element, h } from '@stencil/core';
 
 @Component({
   tag: 'mapa-diary',
@@ -6,6 +6,18 @@ import { Component, ComponentInterface, h } from '@stencil/core';
   shadow: true
 })
 export class Diary implements ComponentInterface {
+
+  @Element()
+  private readonly _elementRef: HTMLMapaDiaryElement;
+
+  componentDidLoad() {
+    this._elementRef.shadowRoot
+      .querySelector('slot')
+      .assignedElements()
+      .forEach((entry, index) => {
+        (entry as HTMLElement).style.setProperty('--diary-entry-index', index.toString());
+      });
+  }
 
   render() {
     return (
